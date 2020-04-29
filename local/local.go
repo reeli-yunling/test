54aa5501b7bfc0d2d750b881466555189b862254
liweimin <lwm@liweimindeMacBook.local>
2020-04-29 09:03:50 +0800

    init

---
+++ local/local.go
@@ -0,0 +1,5 @@
+package local
+
+import (
+	_ "github.com/micro/go-plugins/config/source/grpc/v2"
+)
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+
+	//"github.com/gin-gonic/gin"
+	"net/http"
+
+	"github.com/micro/go-micro/v2/registry"
+	"github.com/micro/go-micro/v2/registry/etcd"
+	"github.com/micro/go-micro/v2/web"
+
+	//	"github.com/micro/go-plugins/config/source/grpc"
+	_ "webir-go/ad/web/test/local"
+
+	"github.com/siddontang/go/log"
+)
+
+func main() {
+
+	reg := etcd.NewRegistry(func(op *registry.Options) {
+		op.Addrs = []string{
+			"http://127.0.0.1:2379",
+		}
+	})
+	service := web.NewService(
+		web.Name("webir:ad:http"),
+		web.Version("0.0.1"),
+		web.Registry(reg),
+		web.Address(":9002"),
+	)
+	//client.InitApiService()
+	//model.Init()
+	//r := gin.Default()
+	//r := routers.NewRouter()
+	//service.Handle("/", r)
+	service.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("Hello!!!"))
+	})
+
+	err := service.Run()
+	if err != nil {
+		log.Info(err)
+	}
+}
